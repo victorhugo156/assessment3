5c92d1072288da7eda1cd38c350c4380f6ad068d
Victor Hugo <victorhugo156@gmail.com>
2026-02-07 20:14:30 +1100

    Working on employee class improvements

--- employee.py
+++ employee.py
@@ -1,4 +1,6 @@
 import random
+import os
+import json
 
 
 class Employee:
@@ -8,9 +10,13 @@ class Employee:
         self.employees = {}
 
     # Method that populates employee dictionary
-    def add_employee(self, employee_name, employee_age, employee_position, employee_salary, employee_location, employee_department):
+    def add_employee(self, employee_name, employee_age, employee_position, employee_salary, employee_location,
+                     employee_department):
+
         employee_id = random.randint(1, 100)
-        self.employees[employee_id] = {
+
+        employee_data = {
+            "__id": employee_id,
             "__name": employee_name,
             "__age": employee_age,
             "__position": employee_position,
@@ -19,7 +25,24 @@ class Employee:
             "department": employee_department,
         }
 
+        self.write_employee(employee_data)
+
+    def write_employee(self, employee: dict):
+        flags = os.O_WRONLY | os.O_CREAT | os.O_TRUNC
+        employee_file_json = ""
+
+        if not "Current_Employees.json":
+            employee_file_json = os.open("Current_Employees.json", flags, 0o644)
+
+        if employee.id in self.employees:
+            print("Employee already exist")
+        else:
+            json_string = json.dumps(employee, indent=4)
+            os.write(employee_file_json, json_string.encode("utf-8"))
+            os.close(employee_file_json)
+
     # Method that display employees
-    def display_employee(self):
-        for employee in self.employees.items():
-            print(employee)
+    def display_employees(self):
+        with open("Current_Employees.json", "r") as file:
+            content = file.read()
+            print(content)
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import employee
+import os
 
 employee1 = employee.Employee()
 
-employee1.add_employee("Victor", 45, "marketing", 50)
+employee1.add_employee("Victor", 45, "manager", 50, "Sydney", "Marketing")
 
-employee1.display_employee()
+employee1.display_employees()
+
